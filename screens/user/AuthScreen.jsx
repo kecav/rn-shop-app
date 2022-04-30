42f50918052de2153eda745ac1ea2faa78d21670
Keshav <keshav.yadav1999@gmail.com>
2022-04-30 09:33:11 +0530

    Updated Login UI

--- screens/user/AuthScreen.jsx
+++ screens/user/AuthScreen.jsx
@@ -18,8 +18,10 @@ const AuthScreen = (props) => {
     const dispatch = useDispatch();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [cPassword, setcPassword] = useState("");
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
+    const [isRegistering, setIsRegistering] = useState(false);
 
     useEffect(() => {
         if (error) {
@@ -31,13 +33,13 @@ const AuthScreen = (props) => {
         setIsLoading(true);
         setError(null);
 
-        if(email.trim().length==0 || password.trim().length==0){
+        if (email.trim().length == 0 || password.trim().length == 0) {
             setError("Please enter valid credentials");
             setIsLoading(false);
             return;
         }
 
-        console.log(method, "EMAIL: ", email, " PASS:", password);
+        // console.log(method, "EMAIL: ", email, " PASS:", password);
         try {
             if (method === "signup") {
                 await dispatch(authActions.signup(email, password));
@@ -47,9 +49,23 @@ const AuthScreen = (props) => {
                 await dispatch(authActions.login(email, password));
             }
         } catch (error) {
+            // console.log(error);
             setError(error.message);
         }
         setIsLoading(false);
+    };
+
+    const register = () => {
+        // console.log("Register");
+        setError(null);
+        if(isRegistering){
+            if(password!=cPassword){
+                setError("Password Mismatch!");
+                return;
+            }
+            authHandler("signup");
+        }
+        setIsRegistering(true);
     };
 
     return (
@@ -59,48 +75,59 @@ const AuthScreen = (props) => {
                 colors={["#0093E9", "#CE9FFC"]}
                 style={styles.background}
             >
-                <View style={styles.inputContainer}>
-                    <TextInput
-                        placeholder="Email"
-                        value={email}
-                        keyboardType="email-address"
-                        onChangeText={(text) => setEmail(text)}
-                        style={styles.input}
-                        autoCapitalize="none"
-                    />
-                    <TextInput
-                        placeholder="Password"
-                        value={password}
-                        keyboardType="default"
-                        onChangeText={(text) => setPassword(text)}
-                        style={styles.input}
-                        secureTextEntry
-                    />
-                </View>
-                {isLoading ? (
-                    <ActivityIndicator size="large" color="#f00" />
-                ) : (
-                    <View style={styles.buttonContainer}>
-                        <TouchableOpacity
-                            onPress={() => {
-                                authHandler("login");
-                            }}
-                            style={styles.button}
-                        >
-                            <Text style={styles.buttonText}>Login</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            onPress={() => {
-                                authHandler("signup");
-                            }}
-                            style={[styles.button, styles.buttonOutline]}
-                        >
-                            <Text style={styles.buttonOutlineText}>
-                                Register
-                            </Text>
-                        </TouchableOpacity>
+                <View style={styles.authContainer}>
+                    <Text style={styles.authText}>AUTHENTICATION</Text>
+                    <View style={styles.inputContainer}>
+                        <TextInput
+                            placeholder="Email"
+                            value={email}
+                            keyboardType="email-address"
+                            onChangeText={(text) => setEmail(text)}
+                            style={styles.input}
+                            autoCapitalize="none"
+                        />
+                        <TextInput
+                            placeholder="Password"
+                            value={password}
+                            keyboardType="default"
+                            onChangeText={(text) => setPassword(text)}
+                            style={styles.input}
+                            secureTextEntry
+                        />
+                        {isRegistering && (
+                            <TextInput
+                                placeholder="Enter password again"
+                                value={cPassword}
+                                keyboardType="default"
+                                onChangeText={(text) => setcPassword(text)}
+                                style={styles.input}
+                                secureTextEntry
+                            />
+                        )}
                     </View>
-                )}
+                    {isLoading ? (
+                        <ActivityIndicator size="large" color="#f00" />
+                    ) : (
+                        <View style={styles.buttonContainer}>
+                            <TouchableOpacity
+                                onPress={() => {
+                                    authHandler("login");
+                                }}
+                                style={styles.button}
+                            >
+                                <Text style={styles.buttonText}>Login</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                onPress={register}
+                                style={[styles.button, styles.buttonOutline]}
+                            >
+                                <Text style={styles.buttonOutlineText}>
+                                    Register
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </View>
             </LinearGradient>
         </KeyboardAvoidingView>
     );
@@ -109,8 +136,7 @@ const AuthScreen = (props) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        backgroundColor: "#dedede",
     },
     inputContainer: {
         width: "80%",
@@ -121,7 +147,9 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
         borderRadius: 10,
         marginTop: 5,
-        fontSize: 16
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: "#dedede",
     },
     buttonContainer: {
         width: "60%",
@@ -152,7 +180,28 @@ const styles = StyleSheet.create({
         fontWeight: "700",
         fontSize: 16,
     },
-    background: { flex: 1, justifyContent: "center", alignItems: "center", width: '100%' },
+    background: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+    },
+    authContainer: {
+        width: "80%",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff",
+        paddingVertical: 50,
+        borderRadius: 20,
+        elevation: 50,
+    },
+    authText: {
+        fontSize: 25,
+        fontWeight: "bold",
+        marginVertical: 20,
+        paddingBottom: 20,
+        fontStyle: "italic",
+    },
 });
 
 export default AuthScreen;
